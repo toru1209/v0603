216e5160dea3e8b0764d18e6f7e09929d98e05f4
ookamiyoujyo <ookamiyoujyo@gmail.com>
2021-06-03 09:33:22 +0900

    Initial commit

---
+++ v0603/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0603
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
+    }
+}
